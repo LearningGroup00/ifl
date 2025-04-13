8b0c6b6622915663150026b333a90fe92d9fc36e
imfaruk <habakana@gmail.com>
2025-04-13 22:44:56 +0600

    update

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Article, type: :model do
   let!(:article) { FactoryBot.create(:article, published: true) }
 
   it "is published" do
+
     expect(article.published).to eq(true)
   end
 end
